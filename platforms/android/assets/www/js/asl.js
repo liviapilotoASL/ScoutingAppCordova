bbc1e46b119ea2d1cc0213179359670956345b55
Will V <will_vauclain@asl.org>
2017-03-04 14:07:33 +0000

    Work from yesterday

--- platforms/android/assets/www/js/asl.js
+++ platforms/android/assets/www/js/asl.js
@@ -5,7 +5,7 @@ function returnToMainPage() {
   })
 }
 
-function handleBackButton() {
+function goBack() {
   console.log("going home")
   
   if($("#game-scouting").is(":visible")) {
@@ -23,6 +23,70 @@ function handleBackButton() {
   }
 }
 
+function clearPitScouting() {
+  $(':input','#pit-scouting')
+  .removeAttr('checked')
+  .removeAttr('selected')
+  .not(':button, :submit, :reset, :hidden, :radio, :checkbox')
+  .val('');
+  
+  $('input:checkbox, #pit-scouting').prop('checked', false);
+  $('#largeImage').attr("src", "");
+}
+
+function getPitDataIfExists(teamName) {
+  var filename = teamName + ".json";
+  
+  var text = null;
+  window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function (dir) {
+    dir.getFile(filename, { create: false }, function (fileEntry) {
+      // The file exists, so read data from it
+      fileEntry.file(function (file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+            refillGameScoutingData(JSON.parse(this.result));
+        };
+
+        reader.readAsText(file);
+
+      });
+    });
+  });
+  
+  return JSON.parse(text);
+}
+
+function getPitScoutingData() {
+  var data = {};
+  data["general"] = {};
+  data["general"]["imgsrc"] = $('#largeImage').attr("src")
+  data["general"]["drivetrain"] = $("#pit-drive-train").val()
+  data["general"]["speed"] = $("#select-speed").val()
+  data["general"]["height"] = $("#select-height").val()
+  
+  data["game"] = {};
+  data["game"]["has-shooter"] = $("#pit-shooter").prop("checked")
+  data["game"]["shooter-type"] = $("#select-shooter").val()
+  data["game"]["ball-collection"] = $("#select-ball").val()
+  data["game"]["can-place-gears"] = $("#pit-gears").prop("checked")
+  data["game"]["can-climb"] = $("#pit-climb").prop("checked")
+  return data
+}
+
+function refillPitScoutingData(data) {
+  $('#largeImage').attr("src", data["general"]["imgsrc"])
+  $("#pit-drive-train").val(data["general"]["drivetrain"])
+  $("#select-speed").val(data["general"]["speed"])
+  $("#select-height").val(data["general"]["height"])
+  
+  $("#pit-shooter").prop("checked", data["game"]["has-shooter"])
+  $("#select-shooter").val(data["game"]["shooter-type"])
+  $("#select-ball").val(data["game"]["ball-collection"])
+  $("#pit-gears").prop(data["game"]["can-place-gears"])
+  $("#pit-climb").prop("checked", data["game"]["can-climb"])
+}
+
 function clearGameScouting() {
   $('.points-total').text(0);
   
@@ -33,6 +97,29 @@ function clearGameScouting() {
   .val('');
   
   $('input:checkbox, #game-scouting').prop('checked', false);
+}
+
+function getGameDataIfExists(teamName, matchNumber) {
+  var filename = "match" + matchNumber + "-" + teamName + ".json";
+  
+  var text = null;
+  window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function (dir) {
+    dir.getFile(filename, { create: false }, function (fileEntry) {
+      // The file exists, so read data from it
+      fileEntry.file(function (file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+            refillGameScoutingData(JSON.parse(this.result));
+        };
+
+        reader.readAsText(file);
+
+      });
+    });
+  });
+  
+  return JSON.parse(text);
 }
 
 function getGameScoutingData() {
@@ -69,17 +156,46 @@ function getGameScoutingData() {
   return data
 }
 
+function refillGameScoutingData(data) {
+  $("#game-scouting-auto-high-goals").text(data["auto"]["high-goals"]);
+  $("#game-scouting-auto-low-goals").text(data["auto"]["low-goals"]);
+  $("#game-scouting-auto-gears").text(data["auto"]["gears"]);
+  $("#game-scouting-auto-baseline").prop("checked", data["auto"]["baseline"]);
+  
+  $("#game-scouting-high-goals").text(data["teleop"]["high-goals"]);
+  $("#game-scouting-low-goals").text(data["teleop"]["low-goals"]);
+  $("#game-scouting-hoppers").text(data["teleop"]["hoppers"]);
+  $("#game-scouting-gears").text(data["teleop"]["gears"]);
+  
+  $("#game-scouting-gears-fouls").text(data["fouls"]["gears"]);
+  $("#game-scouting-human-fouls").text(data["fouls"]["human"]);
+  $("#game-scouting-shooting-fouls").text(data["fouls"]["shooting"]);
+  
+  $("#game-scouting-climbing").prop("checked", data["endgame"]["climbing"]);
+  $("#game-scouting-shooting-after-climbing").prop("checked", data["endgame"]["shooting"]);
+  
+  
+  $("#game-scouting-ship").prop("checked", data["other"]["pilot"]);
+  $("#game-scouting-defense").val(data["other"]["defense"]);
+  $("#game-scouting-fuel-location").val(data["other"]["fuel"]);
+  $("#game-scouting-final-notes").val(data["other"]["notes"]);
+}
+
 $(document).ready(function(){
   $('.asl-game-scouting').click(function(){
   	var teamName = $(this).parent().parent().attr('data-team');
     var matchNumber = $(this).parent().parent().attr('data-match');
+    getGameDataIfExists(teamName, matchNumber);
+    
     $("#game-home").fadeOut(function(){
       $("#game-scouting").fadeIn();
       $("#game-scouting span.team-name").text(teamName);
       $("#game-scouting span.match-number").text(matchNumber);
     });
   });
+  
   $('.navbar-brand').click(returnToMainPage);
+  
   $('.points-element button').click(function(){
   	if ($(this).hasClass('points-up')) {
       var points = parseInt($(this).parent().parent().find('.points-total').html()) + 1;
@@ -98,16 +214,14 @@ $(document).ready(function(){
     var filename = "match" + $("#game-scouting span.match-number").text() + "-" + $("#game-scouting span.team-name").text() + ".json";
     
     window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function (dir) {
-      alert(cordova.file.externalDataDirectory);
       dir.getFile(filename, { create: true }, function (file) {
-        if(confirm("Do you want to overwrite existing scouting data if it exists?"))
-          file.createWriter(function(writer) {
-            writer.write(JSON.stringify(data));
-          })
+        file.createWriter(function(writer) {
+          writer.write(JSON.stringify(data));
+        })
       });
     });
     
-    returnToMainPage();
+    goBack();
     clearGameScouting();
   });
   
@@ -116,7 +230,7 @@ $(document).ready(function(){
     var matchNumber = $(this).parent().parent().attr('data-match');
     $("#pit-home").fadeOut(function(){
       $("#pit-scouting").fadeIn();
-      $("#pit-scouting h2.team-name").text(teamName);
+      $("#team-name").text(teamName);
     });
   });
   
@@ -132,5 +246,45 @@ $(document).ready(function(){
     });
   });
   
-  document.addEventListener("backbutton", handleBackButton, false);
+  $("#pit-scouting-submit").click(function() {
+    // submit form
+    var data = getPitScoutingData();
+    var imgsrc = data["general"]["imgsrc"]
+    
+    var filenameBase = $("#team-name").text();
+    var filenameImg = filenameBase + ".jpg"
+    var filename = filenameBase + ".json"
+    
+    alert(cordova.file.externalDataDirectory + filenameImg)
+    
+    // copy image to data directory
+    var folders = imgsrc.split("/")
+    var enclosingFolder = folders.slice(0, folders.length-1).join("/")
+    var originalImg = folders[folders.length-1]
+    
+    window.resolveLocalFileSystemURL(enclosingFolder, function (dir) {
+      dir.getFile(originalImg, { create: false }, function (file) {
+        window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function (externalDataDir) {
+          file.copyTo(externalDataDir, filenameImg)
+        })
+      });
+    });
+    
+    
+    
+    window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function (dir) {
+      dir.getFile(filename, { create: true }, function (file) {
+        file.createWriter(function(writer) {
+          writer.write(JSON.stringify(data));
+        })
+      });
+    });
+    goBack();
+    clearPitScouting();
+  });
+  
+  $("#game-scouting-clear").click(clearGameScouting)
+  $("#pit-scouting-clear").click(clearPitScouting)
+  
+  document.addEventListener("backbutton", goBack, false);
 });
--- platforms/browser/www/js/photo.js
+++ platforms/browser/www/js/photo.js
@@ -1,84 +1,39 @@
 var pictureSource;   // picture source
-    var destinationType; // sets the format of returned value
+var destinationType; // sets the format of returned value
 
-    // Wait for device API libraries to load
-    //
-    document.addEventListener("deviceready",onDeviceReady,false);
+document.addEventListener("deviceready",onDeviceReady,false);
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    }
+function onDeviceReady() {
+  pictureSource=navigator.camera.PictureSourceType;
+  destinationType=navigator.camera.DestinationType;
 
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoDataSuccess(imageData) {
-      // Uncomment to view the base64-encoded image data
-      // console.log(imageData);
+}
 
-      // Get image handle
-      //
-      var smallImage = document.getElementById('smallImage');
+// Called when a photo is successfully retrieved
+function onPhotoDataSuccess(imageData) {
+  var smallImage = document.getElementById('smallImage');
+  smallImage.style.display = 'block';
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
 
-      // Unhide image elements
-      //
-      smallImage.style.display = 'block';
+// Called when a photo is successfully retrieved
+function onPhotoURISuccess(imageURI) {
+  var largeImage = document.getElementById('largeImage');
+  largeImage.style.display = 'block';
+  largeImage.src = imageURI;
+}
 
-      // Show the captured photo
-      // The in-line CSS rules are used to resize the image
-      //
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }
+// A button will call this function
+$("#capture-photo-button").click(function() {
+    //navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: destinationType.DATA_URL });
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, destinationType: destinationType.FILE_URI });
+    });
 
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoURISuccess(imageURI) {
-      // Uncomment to view the image file URI
-      // console.log(imageURI);
+ $("#upload-photo-button").click(function() {
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, destinationType: destinationType.FILE_URI, sourceType: pictureSource.PHOTOLIBRARY });
+ });
 
-      // Get image handle
-      //
-      var largeImage = document.getElementById('largeImage');
-
-      // Unhide image elements
-      //
-      largeImage.style.display = 'block';
-
-      // Show the captured photo
-      // The in-line CSS rules are used to resize the image
-      //
-      largeImage.src = imageURI;
-    }
-
-    // A button will call this function
-    //
-    function capturePhoto() {
-      // Take picture using device camera and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL });
-    }
-
-    // A button will call this function
-    //
-    function capturePhotoEdit() {
-      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-        destinationType: destinationType.DATA_URL });
-    }
-
-    // A button will call this function
-    //
-    function getPhoto(source) {
-      // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    }
-
-    // Called if something bad happens.
-    //
-    function onFail(message) {
-      alert('Failed because: ' + message);
-    }
+// Called if something bad happens.
+function onFail(message) {
+  alert('Failed because: ' + message);
+}
